0f323fabef7adf6d8d66e1a44de2101e0186a949
马文卿 <529837505@qq.com>
2022-12-02 18:04:47 +0800

    debug finish

--- src/utils/monitor.ts
+++ src/utils/monitor.ts
@@ -4,31 +4,31 @@ const m = (socket) => {
         e: null,
         w: null
     }
-    let cmd = {
-        log: null,
-        error: null,
-        warn: null
-    };
     let data = {
-        cmd: cmd,
-        createTime: Date.now()
+        cmd: 'log',
+        createTime: Date.now(),
+        value: null,
+        type: ''
     }
     s.o = window.console.log;
     s.e = window.console.error;
     s.w = window.console.warn;
     window.console.log = function () {
         s.o.apply(console, arguments);
-        cmd.log = arguments
+        data.value = arguments;
+        data.type = 'log';
         socket.emit("data", data);
     }
     window.console.error = function () {
         s.e.apply(console, arguments);
-        cmd.error = arguments
+        data.value = arguments;
+        data.type = 'error';
         socket.emit("data", data);
     }
     window.console.warn = function () {
         s.w.apply(console, arguments);
-        cmd.warn = arguments
+        data.value = arguments;
+        data.type = 'warn';
         socket.emit("data", data);
     }
 }
