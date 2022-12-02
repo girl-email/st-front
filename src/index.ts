e50ee51aa938e2921cc5bc696c09d06a6d6983a5
马文卿 <529837505@qq.com>
2022-12-02 16:39:02 +0800

    rewrite console

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,22 @@ import { ClientToServerEvents } from "./types/main";
 import m from "./utils/monitor";
 import project from "../package.json";
 
-const link = (window) => {
+const link = () => {
     const socket: Socket<ClientToServerEvents> = io("ws://10.255.8.9:7001");
     console.log(
-        `\n%cst-sdk${project.version}  🚀`,
+        `\n%c${project.name}${project.version}  🚀`,
         'color:#0dd8d8; background:#0b1021; font-size:25px; padding:8px; text-align: center; font-family: Rockwell; border: 2px solid #0dd8d8; border-radius: 4px;font-weight: bold; text-shadow: 1px 1px 1px #00af87bf;'
     );
 
     socket.on("connect", () => {
         console.log('%cst-sdk连接成功', 'color: #fff; background:#3CB371; font-size: 16px; padding:4px; border-radius: 4px;');
+        m(socket);
     });
 
+
     socket.on("data", (arg) => {
-        console.log(arg);
+
     });
-    console.log('link',m(window));
     
     socket.on("disconnect", () => {
         console.log('st-sdk连接关闭');
@@ -26,11 +27,11 @@ const link = (window) => {
     socket.emit("data", "world");
 
     socket.io.on("reconnect", (attempt) => {
-        console.log('st-sdk正在重新连接...');
+        // console.log('st-sdk正在重新连接...');
     });
 
     socket.io.on("reconnect_error", (error) => {
-        console.log('st-sdk重连遇到故障：', error);
+        // console.log('st-sdk重连遇到故障：', error);
     });
 }
 
--- src/utils/monitor.ts
+++ src/utils/monitor.ts
@@ -1,10 +1,33 @@
-const m = (window) => {
+const m = (socket) => {
     let l;
-    
-    window.console.log = function (arg) {
-        l = arg
+    let s = {
+        o: null,
+        e: null,
+        w: null
     }
-    return l
+    let cmd = {
+        log: null,
+        error: null,
+        warn: null
+    };
+    s.o = window.console.log;
+    s.e = window.console.error;
+    s.w = window.console.warn;
+    window.console.log = function () {
+        s.o.apply(console, arguments);
+        cmd.log = arguments
+        socket.emit("data", cmd);
+    }
+    window.console.error = function () {
+        s.e.apply(console, arguments);
+        cmd.error = arguments
+        socket.emit("data", cmd);
+    }
+    window.console.warn = function () {
+        s.w.apply(console, arguments);
+        cmd.warn = arguments
+        socket.emit("data", cmd);
+    }
 }
 
 export default m
