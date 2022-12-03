9b3a3147fff71d7129e1d3287a37adb3ba43fda0
马文卿 <529837505@qq.com>
2022-12-03 22:50:58 +0800

    optimize code

--- src/utils/monitor.ts
+++ src/utils/monitor.ts
@@ -1,8 +1,13 @@
 const m = (socket) => {
+    // let s = {
+    //     o: null,
+    //     e: null,
+    //     w: null
+    // }
     let s = {
-        o: null,
-        e: null,
-        w: null
+        log: null,
+        error: null,
+        warn: null
     }
     let data = {
         cmd: 'log',
@@ -10,27 +15,36 @@ const m = (socket) => {
         value: null,
         type: ''
     }
-    s.o = window.console.log;
-    s.e = window.console.error;
-    s.w = window.console.warn;
-    window.console.log = function () {
-        s.o.apply(console, arguments);
-        data.value = arguments;
-        data.type = 'log';
-        socket.emit("data", data);
-    }
-    window.console.error = function () {
-        s.e.apply(console, arguments);
-        data.value = arguments;
-        data.type = 'error';
-        socket.emit("data", data);
-    }
-    window.console.warn = function () {
-        s.w.apply(console, arguments);
-        data.value = arguments;
-        data.type = 'warn';
-        socket.emit("data", data);
-    }
+    Object.keys(s).forEach(el => {
+        s[el] = window.console[el];
+        window.console[el] = function () {
+            s[el].apply(console, arguments);
+            data.value = arguments;
+            data.type = el;
+            socket.emit("data", data);
+        }
+    })
+    // s.o = window.console.log;
+    // s.e = window.console.error;
+    // s.w = window.console.warn;
+    // window.console.log = function () {
+    //     s.o.apply(console, arguments);
+    //     data.value = arguments;
+    //     data.type = 'log';
+    //     socket.emit("data", data);
+    // }
+    // window.console.error = function () {
+    //     s.e.apply(console, arguments);
+    //     data.value = arguments;
+    //     data.type = 'error';
+    //     socket.emit("data", data);
+    // }
+    // window.console.warn = function () {
+    //     s.w.apply(console, arguments);
+    //     data.value = arguments;
+    //     data.type = 'warn';
+    //     socket.emit("data", data);
+    // }
 }
 
 export default m
